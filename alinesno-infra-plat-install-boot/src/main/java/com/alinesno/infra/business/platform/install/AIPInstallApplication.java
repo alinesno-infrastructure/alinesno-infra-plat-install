7c6ca4c0e632c8f56c65655ae40c2c856ad5395f
李伟(Michael) <michaelAip@gmail.com>
2025-01-10 22:29:25 +0800

    feat:在AIPInstallApplication中排除RedissonAutoConfiguration配置

--- alinesno-infra-plat-install-boot/src/main/java/com/alinesno/infra/business/platform/install/AIPInstallApplication.java
+++ alinesno-infra-plat-install-boot/src/main/java/com/alinesno/infra/business/platform/install/AIPInstallApplication.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.business.platform.install;
 
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -13,7 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @version 1.0.0
  */
 @Slf4j
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) // 不需要加载数据库时
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class , RedissonAutoConfiguration.class}) // 不需要加载数据库时
 @EnableScheduling
 public class AIPInstallApplication {
 
