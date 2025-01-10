5e6f222edbebee08713536e46f76a7ce415c3a1a
梁明树(Nina) <ninaAip@gmail.com>
2025-01-10 20:12:59 +0800

    feat:实现用户登录逻辑并注释掉与token相关的操作

--- alinesno-infra-plat-install-ui/src/store/modules/user.js
+++ alinesno-infra-plat-install-ui/src/store/modules/user.js
@@ -1,12 +1,12 @@
 import { login, logout, getInfo , goSsoAuthUrl , doLoginByTicket , isSsoLogin } from '@/api/login'
-import { getToken, setToken, removeToken } from '@/utils/auth'
+// import { getToken, setToken, removeToken } from '@/utils/auth'
 import defAva from '@/assets/images/profile.jpg'
 
 const useUserStore = defineStore(
   'user',
   {
     state: () => ({
-      token: getToken(),
+      token: '' ,
       name: '',
       avatar: '',
       roles: [],
@@ -53,7 +53,7 @@ const useUserStore = defineStore(
         const uuid = userInfo.uuid
         return new Promise((resolve, reject) => {
           login(username, password, code, uuid).then(res => {
-            setToken(res.token)
+            // setToken(res.token)
             this.token = res.token
             resolve()
           }).catch(error => {
@@ -89,7 +89,7 @@ const useUserStore = defineStore(
             this.token = ''
             this.roles = []
             this.permissions = []
-            removeToken()
+            // removeToken()
             resolve()
           }).catch(error => {
             reject(error)
