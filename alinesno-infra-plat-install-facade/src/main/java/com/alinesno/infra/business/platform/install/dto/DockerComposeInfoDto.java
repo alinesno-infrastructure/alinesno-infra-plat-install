b65b22ed32bd0fe4bbf46052b35831edfe6998fc
李娜(Allen) <allenAip@gmail.com>
2025-01-10 20:12:59 +0800

    feat:新增 DockerComposeInfoDto 类，用于封装docker-compose相关信息。

---
+++ alinesno-infra-plat-install-facade/src/main/java/com/alinesno/infra/business/platform/install/dto/DockerComposeInfoDto.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.business.platform.install.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * docker-compose信息
+ * 
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@ToString
+@Data
+public class DockerComposeInfoDto {
+
+	private String info;
+
+}
