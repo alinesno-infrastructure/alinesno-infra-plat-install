b6b6d860c97429bfb1487b666ec1846c83f8f304
李伟(Michael) <michaelAip@gmail.com>
2025-01-10 22:29:25 +0800

    feat:更新 AipConfigDownUtils 工具类以优化文件下载和解析逻辑

--- alinesno-infra-plat-install-domain/src/main/java/com/alinesno/infra/business/platform/install/utils/AipConfigDownUtils.java
+++ alinesno-infra-plat-install-domain/src/main/java/com/alinesno/infra/business/platform/install/utils/AipConfigDownUtils.java
@@ -25,13 +25,15 @@ public class AipConfigDownUtils {
         FileUtils.forceDeleteOnExit(new File(installPath));
         FileUtils.forceMkdir(new File(installPath));
 
-        String downloadUrl = Const.qiniuDomain + "/" + version + "/aip-config.json" ;
+        String downloadUrl = Const.qiniuDomain + File.separator + version + "/aip-config.json" ;
 
         log.debug("开始下载配置文件:{}" , downloadUrl);
-        NetUtils.download(downloadUrl, installPath) ;
+        NetUtils.download(downloadUrl, installPath + File.separator + "aip-config.json") ;
         log.debug("下载配置文件成功:{}" , installPath);
 
         String aipConfigJsonStr = FileUtils.readFileToString(new File(installPath + File.separator + "aip-config.json") , Charset.defaultCharset());
+        log.debug("开始解析配置文件:\r\n{}" , aipConfigJsonStr);
+
         return JSONObject.parseObject(aipConfigJsonStr , AipBean.class);
     }
 
@@ -40,30 +42,29 @@ public class AipConfigDownUtils {
         String projectDir = installPath + File.separator + name ;
         FileUtils.forceMkdir(new File(projectDir)) ;
 
-        String uiDownloadUrl = Const.qiniuDomain + "/" + version + "/" + name + "kubernetes-admin_v110.yaml" ;
-        String bootDownloadUrl = Const.qiniuDomain + "/" + version + "/" + name + "kubernetes-boot_v110.yaml" ;
+        String bootDownloadUrl = Const.qiniuDomain + File.separator + version + File.separator + name + File.separator +"kubernetes-dev.yaml" ;
+        String uiDownloadUrl = Const.qiniuDomain + File.separator + version + File.separator + name + File.separator +"kubernetes-admin-dev.yaml" ;
 
-        NetUtils.download(projectDir , bootDownloadUrl);
-        NetUtils.download(projectDir , uiDownloadUrl);
+        NetUtils.download(bootDownloadUrl , projectDir + File.separator + "kubernetes-dev.yaml");
+        NetUtils.download(uiDownloadUrl , projectDir + File.separator + "kubernetes-admin-dev.yaml");
 
         log.debug("下载项目:{}文件成功:{}" , name , projectDir);
     }
 
     public void database(String version) {
         log.debug("开始下载数据库文件...");
-        String downloadUrl = Const.qiniuDomain + "/" + version + "/alinesno-database.sql" ;
+        String downloadUrl = Const.qiniuDomain + File.separator + version + "/alinesno-database.sql" ;
 
         log.debug("开始下载配置文件:{}" , downloadUrl);
-        NetUtils.download(downloadUrl, installPath) ;
+        NetUtils.download(downloadUrl, installPath + File.separator + "alinesno-database.sql") ;
         log.debug("下载配置文件成功:{}" , installPath);
     }
 
     public void env(String version) {
-        log.debug("开始下载数据库文件...");
-        String downloadUrl = Const.qiniuDomain + "/" + version + "/alinesno-env-tools.yaml" ;
+        String downloadUrl = Const.qiniuDomain + File.separator + version + "/alinesno-env-tools.yaml" ;
 
         log.debug("开始下载配置文件:{}" , downloadUrl);
-        NetUtils.download(downloadUrl, installPath) ;
+        NetUtils.download(downloadUrl, installPath + File.separator + "alinesno-env-tools.yaml") ;
         log.debug("下载配置文件成功:{}" , installPath);
     }
 }
