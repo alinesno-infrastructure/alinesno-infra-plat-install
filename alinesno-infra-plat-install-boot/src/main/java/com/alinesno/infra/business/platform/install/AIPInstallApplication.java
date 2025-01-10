4a6155eccc99d2580d0b83c7b2ae7918a3bf388d
李娜(Allen) <allenAip@gmail.com>
2025-01-10 20:12:59 +0800

    feat:添加AIPInstallApplication启动类

---
+++ alinesno-infra-plat-install-boot/src/main/java/com/alinesno/infra/business/platform/install/AIPInstallApplication.java
@@ -0,0 +1,24 @@
+package com.alinesno.infra.business.platform.install;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 启动入口
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) // 不需要加载数据库时
+@EnableScheduling
+public class AIPInstallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIPInstallApplication.class, args);
+	}
+	
+}
