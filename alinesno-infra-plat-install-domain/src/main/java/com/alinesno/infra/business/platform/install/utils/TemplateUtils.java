f3a3f9f33617485032cff9e8939abeee60559dcd
李娜(Allen) <allenAip@gmail.com>
2025-01-12 00:27:56 +0800

    feat:在 TemplateUtils 中引入 InstallForm DTO 并注释相关引擎初始化代码

--- alinesno-infra-plat-install-domain/src/main/java/com/alinesno/infra/business/platform/install/utils/TemplateUtils.java
+++ alinesno-infra-plat-install-domain/src/main/java/com/alinesno/infra/business/platform/install/utils/TemplateUtils.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.business.platform.install.utils;
 
 import com.alinesno.infra.business.platform.install.constants.AIPYaml;
+import com.alinesno.infra.business.platform.install.dto.InstallForm;
 import org.apache.commons.io.FileUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -18,78 +19,83 @@ import java.util.Map;
  */
 public class TemplateUtils {
 
-	public static VelocityEngine k8sEngine;
-	public static VelocityEngine dockerEngine;
+//	public static VelocityEngine k8sEngine;
+//	public static VelocityEngine dockerEngine;
+//
+//	static {
+//		String k8sTemplatePath = NetUtils.getInstallFile() ;
+//		k8sEngine = new VelocityEngine();
+//		k8sEngine.setProperty(Velocity.FILE_RESOURCE_LOADER_PATH, k8sTemplatePath);
+//		k8sEngine.setProperty(Velocity.RUNTIME_LOG, NetUtils.getInstallFile() + File.separator + "velocity.log");
+//		k8sEngine.setProperty(Velocity.INPUT_ENCODING, "UTF-8");
+//		k8sEngine.setProperty(Velocity.OUTPUT_ENCODING, "UTF-8");
+//		k8sEngine.init();
+//
+//		String dockerTemplatePath = NetUtils.getInstallFile() ;
+//		dockerEngine = new VelocityEngine();
+//		dockerEngine.setProperty(Velocity.RUNTIME_LOG, NetUtils.getInstallFile() + File.separator + "velocity.log");
+//		dockerEngine.setProperty(Velocity.FILE_RESOURCE_LOADER_PATH, dockerTemplatePath);
+//		dockerEngine.setProperty(Velocity.INPUT_ENCODING, "UTF-8");
+//		dockerEngine.setProperty(Velocity.OUTPUT_ENCODING, "UTF-8");
+//		dockerEngine.init();
+//	}
 
-	static {
-		String k8sTemplatePath = NetUtils.getInstallFile() ;
-		k8sEngine = new VelocityEngine();
-		k8sEngine.setProperty(Velocity.FILE_RESOURCE_LOADER_PATH, k8sTemplatePath);
-		k8sEngine.setProperty(Velocity.RUNTIME_LOG, NetUtils.getInstallFile() + File.separator + "velocity.log");
-		k8sEngine.setProperty(Velocity.INPUT_ENCODING, "UTF-8");
-		k8sEngine.setProperty(Velocity.OUTPUT_ENCODING, "UTF-8");
-		k8sEngine.init();
-		
-		String dockerTemplatePath = NetUtils.getInstallFile() ;
-		dockerEngine = new VelocityEngine();
-		dockerEngine.setProperty(Velocity.RUNTIME_LOG, NetUtils.getInstallFile() + File.separator + "velocity.log");
-		dockerEngine.setProperty(Velocity.FILE_RESOURCE_LOADER_PATH, dockerTemplatePath);
-		dockerEngine.setProperty(Velocity.INPUT_ENCODING, "UTF-8");
-		dockerEngine.setProperty(Velocity.OUTPUT_ENCODING, "UTF-8");
-		dockerEngine.init();
+//	/**
+//	 * 写入模板工具类
+//	 *
+//	 * @param yaml
+//	 * @param config
+//	 * @throws IOException
+//	 */
+//	public static void writeK8STemplate(AIPYaml yaml, Map<String, String> config) throws IOException {
+//
+//		String writePath = NetUtils.getInstallFile() + File.separator +  yaml.getFileName();
+//		Template template = k8sEngine.getTemplate(yaml.getTemplateName()) ;
+//
+//		VelocityContext root = new VelocityContext();
+//
+//		for(String key : config.keySet()) {
+//			root.put(key , config.get(key));
+//		}
+//
+//		File writeFile = new File(writePath) ;
+//
+//		if(writeFile.exists()) {
+//			FileUtils.forceDelete(writeFile);
+//		}
+//
+//		Writer mywriter = new PrintWriter(new FileOutputStream(writeFile));
+//		template.merge(root, mywriter);
+//		mywriter.flush();
+//
+//	}
+//
+//	public static void writeDockerTemplate(AIPYaml yaml, Map<String, String> config) throws IOException {
+//
+//		String writePath = NetUtils.getInstallFile() + File.separator  +  yaml.getFileName();
+//		Template template = dockerEngine.getTemplate(yaml.getTemplateName()) ;
+//
+//		VelocityContext root = new VelocityContext();
+//
+//		for(String key : config.keySet()) {
+//			root.put(key , config.get(key));
+//		}
+//
+//		File writeFile = new File(writePath) ;
+//
+//		if(writeFile.exists()) {
+//			FileUtils.forceDelete(writeFile);
+//		}
+//
+//		Writer mywriter = new PrintWriter(new FileOutputStream(writeFile));
+//		template.merge(root, mywriter);
+//		mywriter.flush();
+//
+//	}
+
+	public static void renderTemplate(String envK8SYaml, InstallForm installForm) {
+
+		// 替换环境变量
+
 	}
-
-	/**
-	 * 写入模板工具类
-	 * 
-	 * @param yaml
-	 * @param config
-	 * @throws IOException
-	 */
-	public static void writeK8STemplate(AIPYaml yaml, Map<String, String> config) throws IOException {
-		
-		String writePath = NetUtils.getInstallFile() + File.separator +  yaml.getFileName();
-		Template template = k8sEngine.getTemplate(yaml.getTemplateName()) ; 
-		
-		VelocityContext root = new VelocityContext();
-		
-		for(String key : config.keySet()) {
-			root.put(key , config.get(key));
-		}
-
-		File writeFile = new File(writePath) ; 
-	
-		if(writeFile.exists()) {
-			FileUtils.forceDelete(writeFile);
-		}
-		
-		Writer mywriter = new PrintWriter(new FileOutputStream(writeFile));
-		template.merge(root, mywriter);
-		mywriter.flush();
-		
-	}
-
-	public static void writeDockerTemplate(AIPYaml yaml, Map<String, String> config) throws IOException {
-		
-		String writePath = NetUtils.getInstallFile() + File.separator  +  yaml.getFileName();
-		Template template = dockerEngine.getTemplate(yaml.getTemplateName()) ; 
-		
-		VelocityContext root = new VelocityContext();
-		
-		for(String key : config.keySet()) {
-			root.put(key , config.get(key));
-		}
-
-		File writeFile = new File(writePath) ; 
-	
-		if(writeFile.exists()) {
-			FileUtils.forceDelete(writeFile);
-		}
-		
-		Writer mywriter = new PrintWriter(new FileOutputStream(writeFile));
-		template.merge(root, mywriter);
-		mywriter.flush();
-		
-	}
-
 }
